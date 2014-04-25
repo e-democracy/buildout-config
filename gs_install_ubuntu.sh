ec877402d790d864d055de94d1eabb9318693e32
Bill Bushey <wbushey@gmail.com>
2014-04-25 09:03:31 -0500

    Attempting to update Zope to 2.13.21

--- gs_install_ubuntu.sh
+++ gs_install_ubuntu.sh
@@ -81,7 +81,7 @@ ln -s /usr/lib/python2.7/dist-packages/libxsltmod.so lib/python2.7/site-packages
 ln -s /usr/lib/python2.7/dist-packages/libxml2mod.so lib/python2.7/site-packages/
 ln -s /usr/lib/python2.7/dist-packages/libxml2.py lib/python2.7/site-packages/
 # Fetch the system that builds GroupServer
-pip install zc.buildout==1.6.3
+pip install zc.buildout==1.7.1
 echo
 echo "Python setup complete."
 
