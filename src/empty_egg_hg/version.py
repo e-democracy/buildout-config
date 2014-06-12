7d5dedaad1c4840d255845cd766c0e0e02384f3e
Bill Bushey <wbushey@gmail.com>
2014-06-12 13:38:03 -0500

    Updated hg version.py based on upstream hg version.py is now Py 3 ready and uses semantic versioning

--- src/empty_egg_hg/version.py
+++ src/empty_egg_hg/version.py
@@ -1,14 +1,13 @@
-#coding=utf-8
-from __future__ import absolute_import, unicode_literals
-
-#-----------------------------------------------------------------------------#
-
-version = '0.98'
+# -*- coding: utf-8 -*-
+version = '2.0.0'
 release = False
 
 #-----------------------------------------------------------------------------#
-
-import commands
+import sys
+if (sys.version_info < (3, )):
+    from commands import getstatusoutput
+else:
+    from subprocess import getstatusoutput  # lint:ok
 import datetime
 import os
 import glob
@@ -19,21 +18,24 @@ class CommandError(Exception):
 
 
 def execute_command(commandstring):
-    status, output = commands.getstatusoutput(commandstring)
+    status, output = getstatusoutput(commandstring)
     if status != 0:
-        raise CommandError
+        m = 'Command "{0}" exited with status {1}'
+        msg = m.format(commandstring, status)
+        raise CommandError(msg)
     return output
 
 
 def parse_version_from_package():
     try:
-        pkginfo = os.path.join(glob.glob('*.egg-info')[0], 'PKG-INFO')
+        pkginfo = os.path.join(glob.glob('*.egg-info')[0],
+                                         'PKG-INFO')
     except:
         pkginfo = ''
 
     version_string = ''
     if os.path.exists(pkginfo):
-        for line in file(pkginfo):
+        for line in open(pkginfo):
             if line.find('Version: ') == 0:
                 version_string = line.strip().split('Version: ')[1].strip()
         if not version_string:
@@ -46,18 +48,28 @@ def parse_version_from_package():
 
 def get_version():
     try:
-        gitLog = execute_command('git log -1 --format="%ct-%H"')
-        (commitdate, dash, commithash) = gitLog.partition('-')
-        dt = datetime.datetime.utcfromtimestamp(float(commitdate))
+        globalid = execute_command("hg identify -i").strip('+')
+        c = "hg log -r %s --template '{date|isodatesec}'" % globalid
+        commitdate = execute_command(c)
+        # convert date to UTC unix timestamp, using the date command because
+        # python date libraries do not stabilise till about 2.6
+        timestamp = int(execute_command('date -d"%s" --utc +%%s' % commitdate))
+
+        # finally we have something we can use!
+        dt = datetime.datetime.utcfromtimestamp(timestamp)
         datestring = dt.strftime('%Y%m%d%H%M%S')
+        if release:
+            version_string = version
+        else:
+            version_string = "%s.dev%s-%s" % (version, datestring, globalid)
 
-        version_string = "%s-%s-%s" % (version, datestring, commithash)
-
-    except CommandError:
+    except (CommandError, ValueError, TypeError):
+        # --=mpj17=-- Usually because we are building out a source-egg, rather
+        # than from a Hg source-directory.
         version_string = parse_version_from_package()
 
     return version_string
 
-
 if __name__ == '__main__':
-    print get_version()
+    import sys
+    sys.stdout.write('{0}\n'.format(get_version()))
