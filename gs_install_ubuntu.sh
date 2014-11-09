7de04f1ff7d76035cdabf29e86f3f0eba40062ab
Bill Bushey <wbushey@gmail.com>
2014-11-08 19:43:40 -0600

    A bunch of upstream changes from the 14.06 and (probable) 14.11 Groupserver Releases

--- gs_install_ubuntu.sh
+++ gs_install_ubuntu.sh
@@ -1,37 +1,42 @@
 #!/bin/bash
 
+# First ensure we are *NOT* *ROOT*
+if [[ $(whoami) == 'root' ]]
+then
+  echo "The installation should be done as a normal user,"
+  echo "any user other than root."
+  exit
+fi
+
 . read_ini.sh
 read_ini config.cfg config --prefix CFG
 
-echo "Installing dependencies"
+echo "Installing dependencies:"
+echo "You will need to authenticate so this script can do this."
 echo
-sudo apt-get install -y python python-virtualenv python-dev build-essential postgresql libpq-dev swaks redis-server libxslt-dev libxml2 python-libxslt1 libjpeg8-dev
+sudo apt-get install -y sed python python-virtualenv python-dev build-essential postfix postgresql libpq-dev swaks redis-server libxslt-dev libjpeg62-dev
 
 echo
 echo "Testing mail setup..."
 echo
-for tmail in ${CFG__config__admin_email} ${CFG__config__user_email}
-do
-    if [ "${CFG__config__smtp_user}" != "" ]; then
-        swaks -S -s ${CFG__config__smtp_host} -p ${CFG__config__smtp_port} --from ${CFG__config__support_email} --to ${tmail} --auth-user ${CFG__config__smtp_user}  --auth-pass ${CFG__config__smtp_password}
-    else
-        swaks -S -s ${CFG__config__smtp_host} -p ${CFG__config__smtp_port} --from ${CFG__config__support_email} --to ${tmail}
-    fi
-
-    if [ $? -eq 0 ]; then
-        echo "Mail setup works. Expect an email at ${tmail}"
-        echo "from ${CFG__config__support_email}."
-        echo
-    else
-        echo
-        echo
-        echo "There was a problem with the mail configuration. Either install"
-        echo "Postfix:"
-        echo "    sudo apt-get install postfix"
-        echo "Or specify a remote SMTP server in the 'config.cfg' file."
-        exit
-    fi
-done
+if [ "${CFG__config__smtp_user}" != "" ]; then
+    swaks -S -s ${CFG__config__smtp_host} -p ${CFG__config__smtp_port} --from ${CFG__config__support_email} --to ${CFG__config__admin_email} --auth-user ${CFG__config__smtp_user}  --auth-pass ${CFG__config__smtp_password}
+else
+    swaks -S -s ${CFG__config__smtp_host} -p ${CFG__config__smtp_port} --from ${CFG__config__support_email} --to ${CFG__config__admin_email}
+fi
+if [ $? -eq 0 ]; then
+    echo "Mail setup works. Expect an email at ${CFG__config__admin_email}"
+    echo "from ${CFG__config__support_email}."
+    echo
+else
+    echo
+    echo
+    echo "There was a problem with the mail configuration. Either install"
+    echo "Postfix:"
+    echo "    sudo apt-get install postfix"
+    echo "Or specify a remote SMTP server in the 'config.cfg' file."
+    exit
+fi
 
 echo
 echo "Testing the network..."
@@ -47,25 +52,39 @@ else
 fi
 
 # initialise PostgreSQL database
+echo
 echo "Setting up the databases"
-sudo su -l -c"createuser -D -S -R -l ${CFG__config__pgsql_user}" postgres
-sudo su -l -c"createuser -D -S -R -l ${CFG__config__relstorage_user}" postgres
+echo
+sudo su -l -c"createuser -p ${CFG__config__pgsql_port} -d -S -R -l ${CFG__config__pgsql_user}" postgres
+sudo su -l -c"createuser -p ${CFG__config__relstorage_port} -d -S -R -l ${CFG__config__relstorage_user}" postgres
 
-sudo su -l -c"createdb -Ttemplate0 -O ${CFG__config__pgsql_user} -EUTF-8 ${CFG__config__pgsql_dbname}" postgres
-sudo su -l -c"createdb -Ttemplate0 -O ${CFG__config__relstorage_user} -EUTF-8 ${CFG__config__relstorage_dbname}" postgres
+sudo su -l -c"createdb -p ${CFG__config__pgsql_port} -Ttemplate0 -O ${CFG__config__pgsql_user} -EUTF-8 ${CFG__config__pgsql_dbname}" postgres
+sudo su -l -c"createdb -p ${CFG__config__relstorage_port} -Ttemplate0 -O ${CFG__config__relstorage_user} -EUTF-8 ${CFG__config__relstorage_dbname}" postgres
 
-sudo su -l -c"echo \"alter user ${CFG__config__pgsql_user} with encrypted password '${CFG__config__pgsql_password}';\" | psql" postgres
-sudo su -l -c"echo \"alter user ${CFG__config__relstorage_user} with encrypted password '${CFG__config__relstorage_password}';\" | psql" postgres
+sudo su -l -c"echo \"alter user ${CFG__config__pgsql_user} with encrypted password '${CFG__config__pgsql_password}';\" | psql -p ${CFG__config__pgsql_port}" postgres
+sudo su -l -c"echo \"alter user ${CFG__config__relstorage_user} with encrypted password '${CFG__config__relstorage_password}';\" | psql -p ${CFG__config__relstorage_port}" postgres
 
-sudo su -l -c"echo \"grant all privileges on database ${CFG__config__pgsql_dbname} to ${CFG__config__pgsql_user};\" | psql" postgres
-sudo su -l -c"echo \"grant all privileges on database ${CFG__config__relstorage_dbname} to ${CFG__config__relstorage_user};\" | psql" postgres
+sudo su -l -c"echo \"grant all privileges on database ${CFG__config__pgsql_dbname} to ${CFG__config__pgsql_user};\" | psql -p ${CFG__config__pgsql_port}" postgres
+sudo su -l -c"echo \"grant all privileges on database ${CFG__config__relstorage_dbname} to ${CFG__config__relstorage_user};\" | psql -p ${CFG__config__relstorage_port}" postgres
+
+echo -n "Checking max_prepared_transactions database setting... "
+MAX_PREPARED_TRANSACTIONS=`sudo su -l -c "psql -p ${CFG__config__pgsql_port} -c 'SHOW max_prepared_transactions;'" postgres | sed -n -r -e 's/^\s*([[:digit:]]+).*$/\1/p'`
+POSTGRES_CONFIG_FILE=`sudo su -l -c "psql -p ${CFG__config__pgsql_port} -c 'SHOW config_file;'" postgres | sed -n -r -e '3p' | tr -d ' '`
+if [ $MAX_PREPARED_TRANSACTIONS -eq 0 ]; then
+    echo "Prepared Transactions not enabled"
+    echo "Enabling Prepared Transactions in database"
+    sudo sed -i -e 's/^.*max_prepared_transactions = .*$/max_prepared_transactions = 10 \t\t# Set by GroupServer install script/' $POSTGRES_CONFIG_FILE
+    sudo service postgresql restart
+else
+    echo "Prepared Transactions already enabled"
+fi
 
 # This is not the work of angels, but...
 echo "${CFG__config__pgsql_host}:${CFG__config__pgsql_port}:${CFG__config__pgsql_dbname}:${CFG__config__pgsql_user}:${CFG__config__pgsql_password}" > pgpass-tmp
 chmod 600 pgpass-tmp
 export PGPASSFILE=pgpass-tmp
-# It works.
-createlang -U${CFG__config__pgsql_user} -h${CFG__config__pgsql_host} -p${CFG__config__pgsql_port} plpgsql ${CFG__config__pgsql_dbname}
+# ...it works.
+createlang -h${CFG__config__pgsql_host} -p${CFG__config__pgsql_port} -U${CFG__config__pgsql_user} plpgsql ${CFG__config__pgsql_dbname}
 echo
 echo "Databases created"
 echo
@@ -75,14 +94,8 @@ echo
 # Create the Python environment
 virtualenv --no-site-packages . 
 . ./bin/activate
-#echo -e "import sys\n\nsys.setdefaultencoding('utf-8')" > lib/python2.7/site-packages/sitecustomize.py
-#ln -s /usr/lib/python2.7/dist-packages/libxslt.py lib/python2.7/site-packages/libxslt.py
-#ln -s /usr/lib/python2.7/dist-packages/libxsltmod.so lib/python2.7/site-packages/
-#ln -s /usr/lib/python2.7/dist-packages/libxml2mod.so lib/python2.7/site-packages/
-#ln -s /usr/lib/python2.7/dist-packages/libxml2.py lib/python2.7/site-packages/
 # Fetch the system that builds GroupServer
-pip install -U pip
-pip install -U zc.buildout setuptools distribute mr.developer
+pip install zc.buildout==1.7.1
 echo
 echo "Python setup complete."
 
